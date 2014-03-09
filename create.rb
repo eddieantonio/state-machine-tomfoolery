63ead721590d0b48260580064d5c74d4e19d5a26
Eddie Antonio Santos <easantos@ualberta.ca>
2014-03-09 11:47:11 -0600

    Extracted dot template to erb file; Ruby formatting tweaks.

--- create.rb
+++ create.rb
@@ -13,7 +13,7 @@ require 'set'
 # Convert a string into a set of all of its elements. I might have stolen this
 # from Python.
 def frozenset(str)
-    str.split('').to_set
+  str.split('').to_set
 end
 
 # The various sets that exist.
@@ -25,115 +25,126 @@ C = frozenset('C')
 GTA = frozenset('GTA')
 OPROTEIN = PROTEIN - (DNA | RNA)
 
+
 State = Struct.new(:accept, :transitions) do
-    # This is a bad name for the method but I don't know what to call it.
-    def unwrap
-        result = {}
+  # This is a bad name for the method but I don't know what to call it.
+  def unwrap
+    result = {}
 
-        # Populate the hash with each transition.
-        transitions.each do | chars, target_num |
-            chars.each { | char | result[char] = target_num }
-        end
-
-        # Add all missing letters.
-        ('A'..'Z').each do | char |
-            result[char] = nil unless result.has_key? char
-        end
-
-        result.sort_by { |char, _|  char }
+    # Populate the hash with each transition.
+    transitions.each do |chars, target_num|
+      chars.each { |char| result[char] = target_num }
     end
+
+    # Add all missing letters.
+    ('A'..'Z').each do |char|
+      result[char] = nil unless result.has_key? char
+    end
+
+    result.sort_by { |char, _|  char }
+  end
 end
 
+
 STATES = [
-    # Initial state.
-    State.new(:INVALID, {
-        OPROTEIN => 1,
-        GTA => 2,
-        U => 3,
-        C => 4
-    }),
+  # Initial state.
+  State.new(:INVALID, {
+    OPROTEIN => 1,
+    GTA => 2,
+    U => 3,
+    C => 4
+  }),
 
-    # Protein state 1
-    State.new(:PROTEIN, {
-        PROTEIN => 1
-    }),
+  # Protein state 1
+  State.new(:PROTEIN, {
+    PROTEIN => 1
+  }),
 
-    # DNA => state 2 => "GTA"
-    State.new(:DNA, {
-        OPROTEIN => 1,
-        C => 4,
-        U => 3,
-        GTA => 2
-    }),
+  # DNA => state 2 => "GTA"
+  State.new(:DNA, {
+    OPROTEIN => 1,
+    C => 4,
+    U => 3,
+    GTA => 2
+  }),
 
-    # RNA state 3
-    State.new(:RNA, {
-        RNA => 3,
-    }),
+  # RNA state 3
+  State.new(:RNA, {
+    RNA => 3,
+  }),
 
-    # DNA state 4 => "GTCA"
-    State.new(:DNA, {
-        DNA => 4,
-        OPROTEIN => 1
-    })
+  # DNA state 4 => "GTCA"
+  State.new(:DNA, {
+    DNA => 4,
+    OPROTEIN => 1
+  })
 ]
 
 
 
 def run!
-    unless ARGV.member? '-g'
-        write_c_file
-    else
-        write_dot_graph
-    end
+  unless ARGV.member? '-g'
+    write_c_file
+  else
+    write_dot_graph
+  end
 end
 
 
 
 # Prints a Graphviz/Dot graph file.
 def write_dot_graph
-    def state_name(index)
-        "S#{index}"
+  def state_name(index)
+    "S#{index}"
+  end
+
+  # Create a list of accepting state names.
+  accepting = []
+  STATES.each_with_index do |state, num|
+    accepting.push [state_name(num), state] if state.accept != :INVALID
+  end
+
+  # Transitions:
+  transitions = []
+  STATES.each_with_index do |state, num|
+    start = state_name(num)
+
+    for transition, target_num in state.transitions
+      target = state_name(target_num)
+      # Convert the transition set to a nice sorted string of bases.
+      chars = transition.to_a.sort.join ''
+
+      transitions.push [start, target, chars]
     end
+  end
 
-    # Create a list of accepting state names.
-    accepting = []
-    STATES.each_with_index do |state, num|
-        accepting.push state_name(num) if state.accept != :INVALID
-    end
-
-    # Header with formatting options and accepting states.
-    puts <<EOF
-digraph {
-    rankdir=LR;
-    node [shape = doublecircle]
-        #{accepting.join ' '};
-    node [shape = circle];
-EOF
-
-    # Transitions:
-    STATES.each_with_index do |state, num|
-        start = state_name(num)
-
-        state.transitions.each do |transition, target_num|
-            target = state_name(target_num)
-            # Convert the transition set to a nice sorted string of bases.
-            chars = transition.to_a.sort.join ''
-
-            puts "    #{start} -> #{target} [ label = \"#{chars}\"];"
-        end
-    end
-
-    puts '}'
-
+  # Print the actual template.
+  template = load_erb('./states.dot.erb')
+  vars = {accepting_states: accepting, transitions: transitions}
+  template.run binding_from_hash(vars)
 end
+
+
 
 # Writes the C header file to stdout.
 def write_c_file
-    template_file = File.new('./states.c.erb').read
-    template = ERB.new(template_file, nil, '<>')
-    namespace = OpenStruct.new states: STATES
-    template.run namespace.instance_eval { binding }
+  template = load_erb('./states.c.erb')
+  template.run binding_from_hash({states: STATES})
+end
+
+
+
+# Creates a binding suitable for ERB templates from the given hash.
+def binding_from_hash(hash)
+  # Use this as our binding:
+  namespace = OpenStruct.new hash
+  namespace.instance_eval { binding }
+end
+
+# Returns the ERB template with the given filename.
+def load_erb(filename)
+  template_file = File.new(filename).read
+  ERB.new(template_file, nil, '<>')
 end
 
 run! if __FILE__ == $0
